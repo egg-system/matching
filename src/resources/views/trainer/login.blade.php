303a6f0fce63ba2672d2a080a61c8ad11ed6cbac
haji33 <hajime.sasanuma.0828@gmail.com>
2020-05-18 21:16:15 +0900

    refactor: viewのファイルをキャメルケースに

--- src/resources/views/trainer/login.blade.php
+++ src/resources/views/trainer/login.blade.php
@@ -1,5 +1,5 @@
 @extends('trainer.layouts.app')
 
 @section('content')
-    @include('common._login_form', ['is_gym_owner' => false])
+    @include('common._loginForm', ['is_gym_owner' => false])
 @endsection
