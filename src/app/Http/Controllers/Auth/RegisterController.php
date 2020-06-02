fb33254ff307beea4a0ad5eadcfbfd9ab3dc0ece
haji33 <hajime.sasanuma.0828@gmail.com>
2020-06-02 22:58:17 +0900

    feat: 仮登録時にはメールの再送を可能に

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\Login;
 use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class RegisterController extends Controller
@@ -39,14 +38,9 @@ class RegisterController extends Controller
     /**
      * Handle a registration request for the application.
      * メールアドレス登録でログインしないようにオーバーライド
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $this->validator($request->all())->validate();
-
         event(new Registered($user = $this->create($request->all())));
 
         if ($response = $this->registered($request, $user)) {
@@ -58,19 +52,6 @@ class RegisterController extends Controller
             : redirect($this->redirectPath());
     }
 
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:login'],
-        ]);
-    }
 
     /**
      * Create a new user instance after a valid registration.
---
+++ src/app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Models\Login;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|string|email|max:255'
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'email' => 'メールアドレス'
+        ];
+    }
+
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator) {
+            $login = Login::where('email', $this->input('email'))->first();
+            
+            // すでに本登録済みの場合はエラー
+            if ($login !== null && $login->email_verified_at !== null){
+                throw ValidationException::withMessages([
+                    'email' => [trans('validation.custom.auth.register.unique')],
+                ]);
+            }
+        });
+    }
+}
