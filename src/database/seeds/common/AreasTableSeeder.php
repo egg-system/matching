f1bbe8035dca4c29e8a77bc056ff08fde65ca70f
haji33 <hajime.sasanuma.0828@gmail.com>
2020-06-05 00:31:33 +0900

    refactor: HeartRailsExpressClientの作成

--- src/database/seeds/common/AreasTableSeeder.php
+++ src/database/seeds/common/AreasTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeds\Common;
 
+use App\Http\Clients\HeartRailsExpressClient;
 use App\Models\Area;
 use Illuminate\Database\Seeder;
 use GuzzleHttp\Client;
@@ -11,9 +12,8 @@ class AreasTableSeeder extends Seeder
     public function run()
     {
         // 駅情報取得APIを利用して山手線の駅名を取得
-        $client = new Client();
-        $url = 'http://express.heartrails.com/api/json?method=getStations&line=JR%E5%B1%B1%E6%89%8B%E7%B7%9A';
-        $response = $client->request('GET', $url);
+        $client = new HeartRailsExpressClient();
+        $response = $client->getYamanoteLineStations();
         $body = $response->getBody();
         $data = json_decode($body, true);
         $stations = $data['response']['station'];
