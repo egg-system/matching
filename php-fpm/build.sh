796b4c5b4eeea9dcf5229eef72b28ffcdbf426a2
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-04 02:55:23 +0900

    fix : コメントアウトを戻す

--- php-fpm/build.sh
+++ php-fpm/build.sh
@@ -3,9 +3,9 @@ set -e
 
 php artisan migrate --seed
 php artisan config:cache
-# php artisan route:cache
+php artisan route:cache
 php artisan view:cache
 php artisan event:cache
-# php artisan optimize
+php artisan optimize
 
 php-fpm
