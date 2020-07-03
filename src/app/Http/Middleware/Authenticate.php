402237ec7e180d82f639dad9c1aff15b895b348f
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-04 03:01:23 +0900

    fix : bladeのrouteを修正

--- src/app/Http/Middleware/Authenticate.php
+++ src/app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return route('trainer.login');
+            return route('trainer.login.view');
         }
     }
 }
