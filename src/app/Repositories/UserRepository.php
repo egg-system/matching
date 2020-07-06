1b2ceb3f4912ba7a72180ca44eabe501e108fc84
Daisuke Ogata <daisule.ogata.work@gmail.com>
2020-07-06 10:57:30 +0900

    #60 ユーザー関連処理の引数としてRequestを渡すよう修正

--- src/app/Repositories/UserRepository.php
+++ src/app/Repositories/UserRepository.php
@@ -5,8 +5,8 @@ namespace App\Repositories;
 use App\Models\Login;
 use App\Models\MatchingCondition;
 use App\Models\Trainer;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -17,37 +17,38 @@ class UserRepository
      * @param array $params
      * @return mixed
      */
-    public function createTrainer(array $params)
+    public function create(Request $request)
     {
-        return DB::transaction(function () use ($params) {
+        return DB::transaction(function () use ($request) {
+            $validated = $request->validated();
             // トレーナー登録
-            $registered_trainer = Trainer::create($params);
+            $registered_trainer = Trainer::create($validated);
             // matchingConditionと紐付け
-            $registered_trainer->matchingCondition()->create($params);
+            $registered_trainer->matchingCondition()->create($validated);
             // トレーナーとログインの紐付けて、カラムの更新
-            return $registered_trainer->associateToLogin(Login::find($params['id']), [
-                'name' => $params['name'],
+            return $registered_trainer->associateToLogin(Login::find($request->id), [
+                'name' => $request->name,
                 'email_verified_at' => now(),
-                'password' => $params['password']
+                'password' => $request->password
             ]);
         });
     }
 
     /**
      * ユーザー更新
-     * @param array $params
-     * @param Model $user
+     * @param Request $request
+     * @param User $user
      */
-    public function updateUser(array $params, Model $user)
+    public function updateUser(Request $request, User $user)
     {
-        DB::transaction(function () use ($params, $user) {
+        DB::transaction(function () use ($request, $user) {
             // トレーナー更新
-            $user->update(Arr::only($params, $user->getFillable()));
+            $user->update($request->only($user->getFillable()));
             // matchingCondition更新
-            $user->matchingCondition->update(Arr::only($params, MatchingCondition::make()->getFillable()));
+            $user->matchingCondition->update($request->only(MatchingCondition::make()->getFillable()));
             // ログインと紐付けて、カラムの更新
             return $user->associateToLogin(Auth::user(), [
-                'name' => $params['name']
+                'name' => $request->name
             ]);
         });
     }
