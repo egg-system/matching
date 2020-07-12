11ad201020bd7066c90f10dc05cd5cf980aa6b5a
haji33 <hajime.sasanuma.0828@gmail.com>
2020-07-12 23:35:18 +0900

    feat: 新規作成したcomponentのstorybookを作成

---
+++ src/resources/js/components/molecules/form/InputForm.stories.js
@@ -0,0 +1,21 @@
+import FormWrapper from './FormWrapper.vue'
+import InputForm from './InputForm.vue'
+
+export default { title: 'Form/InputForm' };
+
+export const base = () => ({
+    components: { FormWrapper, InputForm },
+    template: `<form-wrapper
+        label="氏名"
+        name="name"
+    >
+        <input-form
+            name="name"
+            id="name"
+            type="text"
+            value="山田太郎"
+            autocomplete="name"
+            autofocus
+        ></input-form>
+    </form-wrapper>`
+})
---
+++ src/resources/js/components/molecules/form/TextAreaForm.stories.js
@@ -0,0 +1,18 @@
+import FormWrapper from './FormWrapper.vue'
+import TextAreaForm from './TextAreaForm.vue'
+
+export default { title: 'Form/TextAreaForm' };
+
+export const base = () => ({
+    components: { FormWrapper, TextAreaForm },
+    template: `<form-wrapper
+        label="自己紹介"
+        name="pr_comment"
+    >
+        <text-area-form
+            name="pr_comment"
+            id="pr_comment"
+            placeholder="自己紹介を入れて企業にアピールしよう"
+        ></text-area-form>
+    </form-wrapper>`
+})
