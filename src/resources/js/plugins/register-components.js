e07a81a0778f5f9fa202dec9fe43ec2ebf58603c
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-15 01:49:10 +0900

    feat : organismの登録

--- src/resources/js/plugins/register-components.js
+++ src/resources/js/plugins/register-components.js
@@ -6,5 +6,13 @@ export const globalComponents = [
   {
     name: 'landing-page-first-view',
     object: () => import('../organisms/landing-page/first-view.vue')
+  },
+  {
+    name: 'landing-page-descriptions',
+    object: () => import('../organisms/landing-page/descriptions.vue')
+  },
+  {
+    name: 'landing-page-conversion',
+    object: () => import('../organisms/landing-page/conversion.vue')
   }
 ]
