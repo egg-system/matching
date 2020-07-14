4326af5c5096a0d9c628e64cd8598d9d90607b23
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-14 22:35:37 +0900

    feat : コンポーネントの登録

--- src/resources/js/plugins/register-components.js
+++ src/resources/js/plugins/register-components.js
@@ -2,5 +2,9 @@ export const globalComponents = [
   {
     name: 'landing-page-header',
     object: () => import('../organisms/landing-page/header.vue')
+  },
+  {
+    name: 'landing-page-first-view',
+    object: () => import('../organisms/landing-page/first-view.vue')
   }
 ]
