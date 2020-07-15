42ed5617cbec3f7b83a1f4e9da5ce7d1e24475cd
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-16 06:44:03 +0900

    fix : コンポーネントの登録方法の修正

--- src/resources/js/plugins/register-components.js
+++ src/resources/js/plugins/register-components.js
@@ -1,22 +1,4 @@
-export const globalComponents = [
-  {
-    name: 'landing-page-header',
-    object: () => import('../organisms/landing-page/header.vue')
-  },
-  {
-    name: 'landing-page-first-view',
-    object: () => import('../organisms/landing-page/first-view.vue')
-  },
-  {
-    name: 'landing-page-descriptions',
-    object: () => import('../organisms/landing-page/descriptions.vue')
-  },
-  {
-    name: 'landing-page-conversion',
-    object: () => import('../organisms/landing-page/conversion.vue')
-  },
-  {
-    name: 'landing-page-questions',
-    object: () => import('../organisms/landing-page/often-questions.vue')
-  }
-]
+import { landingPageComponents } from './components/landing-page-components'
+const globalComponents = []
+
+export const components = globalComponents.concat(landingPageComponents)
