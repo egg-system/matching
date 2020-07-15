64c81ade47450751bf8166be3d8ca0c94b1299c3
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-07-16 06:41:44 +0900

    feat : footerの追加

--- src/resources/views/pages/landing.blade.php
+++ src/resources/views/pages/landing.blade.php
@@ -1,8 +1,37 @@
 @extends('templates.landing-page')
 
+@php
+    // TODO: #117 メニュー用の変数の管理方法を検討する
+    $footerMenus = [
+        [
+            'name' => '運営会社',
+            'url' => '/'
+        ],
+        [
+            'name' => 'お問い合わせ',
+            'url' => '/'
+        ],
+        [
+            'name' => '利用規約',
+            'url' => '/'
+        ],
+        [
+            'name' => '個人情報保護方針',
+            'url' => '/'
+        ],
+        [
+            'name' => '特定商取引法に基づく表記',
+            'url' => '/'
+        ]
+    ];
+@endphp
+
 @section('content')
-<landing-page-first-view></landing-page-first-view>
-<landing-page-descriptions></landing-page-descriptions>
-<landing-page-questions></landing-page-questions>
-<landing-page-conversion></landing-page-conversion>
+    <landing-page-first-view></landing-page-first-view>
+    <landing-page-descriptions></landing-page-descriptions>
+    <landing-page-questions></landing-page-questions>
+    <landing-page-conversion></landing-page-conversion>
+    <landing-page-footer
+        :menus='@json($footerMenus)'
+    ></landing-page-footer>
 @endsection
