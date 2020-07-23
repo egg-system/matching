669db773b69fc3a2f279ae0d463eb0f2a69b1c1e
hk-pvt <gmxfw068@gmail.com>
2020-07-23 15:15:39 +0900

    FIX：エリアデータを階層構造に修正

--- src/app/Models/Area.php
+++ src/app/Models/Area.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Area extends Model
 {
+    use NodeTrait;
+
     protected $fillable = [
         'name',
     ];
