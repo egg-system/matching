fb561806df89d90e864aa0dae313ce571c393d46
hk-pvt <gmxfw068@gmail.com>
2020-07-27 00:49:09 +0900

    update: コールバックを1つに集約

--- src/database/migrations/2020_07_18_151220_change_column_gyms_table.php
+++ src/database/migrations/2020_07_18_151220_change_column_gyms_table.php
@@ -16,28 +16,16 @@ class ChangeColumnGymsTable extends Migration
         // 不要なカラム削除
         Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('president_name');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('staff_count');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('customer_count');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('requirements');
         });
 
         // カラム追加
         Schema::table('gyms', function (Blueprint $table) {
             $table->json('profiles')->nullable()->comment('基本情報（代表者名、従業員数、住所-市区町村、住所-丁・番地以下）');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->unsignedBigInteger("prefecture_id")->nullable()->comment('住所 - 都道府県');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->text("gym_url")->nullable()->comment('店舗URL');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->text("description")->nullable()->comment('紹介文');
         });
     }
@@ -52,28 +40,16 @@ class ChangeColumnGymsTable extends Migration
         // カラム追加
         Schema::table('gyms', function (Blueprint $table) {
             $table->string('president_name')->comment('代表者氏名');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->unsignedInteger('staff_count')->comment('スタッフ数');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->unsignedInteger('customer_count')->nullable()->comment('顧客数');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->json('requirements')->nullable()->comment('募集要項（人数、スキルなど）');
         });
 
         // カラム削除
         Schema::table('gyms', function (Blueprint $table) {            
             $table->dropColumn('profiles');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('prefecture_id');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('gym_url');
-        });
-        Schema::table('gyms', function (Blueprint $table) {
             $table->dropColumn('description');
         });
     }
