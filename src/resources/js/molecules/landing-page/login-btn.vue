58845d38d2129da7ceb4c973dab3b81612e38d83
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-01 13:54:26 +0900

    feat : ログインボタンの機能とデザインを分離

---
+++ src/resources/js/molecules/landing-page/login-btn.vue
@@ -0,0 +1,49 @@
+<template>
+  <main-btn
+    :href="href"
+    :label="label"
+    @click="openDialog"
+  />
+</template>
+
+<script> 
+import mainBtn from '../../atoms/main-btn'
+
+export default {
+  components: { mainBtn },
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  computed: {
+    label() {
+      if (this.isEnabledLogin) {
+        return `無料で始める / ログイン`
+      }
+
+      return '無料で始める'
+    },
+    href() {
+      if (this.isEnabledLogin) {
+        return ''
+      }
+
+      return 'https://forms.gle/TwWSKiC2nxgM4kb99'
+    },
+    isEnabledLogin() {
+      return this.$root.isEnabled('login')
+    }
+  },
+  methods: {
+    openDialog() {
+      if (!this.isEnabledLogin) {
+        return
+      }
+
+      this.$emit('input', true)
+    }
+  }
+}
+</script>
