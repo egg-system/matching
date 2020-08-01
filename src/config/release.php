939765a92aeefa2e36ac181483c40bd45977db8a
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-02 07:51:53 +0900

    feat : フラグの追加

--- src/config/release.php
+++ src/config/release.php
@@ -4,6 +4,9 @@
  *  is_enableがtureの場合は、リリース済みとみなす。
  */
 return [
+    'register' => [
+        'is_enabled' => env('IS_RELEASE_REGISTER', false)
+    ],
     'login' => [
         'is_enabled' => env('IS_RELEASE_LOGIN', false)
     ]
