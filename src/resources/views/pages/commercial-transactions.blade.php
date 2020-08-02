a943c0e9510c885098e9c785dfeb03d3c8b75b4e
Daisuke Ogata <daisule.ogata.work@gmail.com>
2020-08-02 14:51:24 +0900

    #120 インデント修正

--- src/resources/views/pages/commercial-transactions.blade.php
+++ src/resources/views/pages/commercial-transactions.blade.php
@@ -4,24 +4,24 @@
 // TODO: #117 メニュー用の変数の管理方法を検討する
 $footerMenus = [
     [
-    'name' => '運営会社',
-    'url' => 'https://eggsystem.co.jp/'
+        'name' => '運営会社',
+        'url' => 'https://eggsystem.co.jp/'
     ],
     [
-    'name' => 'お問い合わせ',
-    'url' => 'https://forms.gle/ETkFKttFfxLPM6DF6'
+        'name' => 'お問い合わせ',
+        'url' => 'https://forms.gle/ETkFKttFfxLPM6DF6'
     ],
     [
-    'name' => '利用規約',
-    'url' => '/service-term'
+        'name' => '利用規約',
+        'url' => '/service-term'
     ],
     [
-    'name' => '個人情報保護方針',
-    'url' => '/privacy-policy'
+        'name' => '個人情報保護方針',
+        'url' => '/privacy-policy'
     ],
     [
-    'name' => '特定商取引法に基づく表記',
-    'url' => '/commercial-transactions'
+        'name' => '特定商取引法に基づく表記',
+        'url' => '/commercial-transactions'
     ]
 ];
 @endphp
