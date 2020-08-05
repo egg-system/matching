33691241f2f77da6b907a2eba96098847456f762
Daisuke Ogata <daisule.ogata.work@gmail.com>
2020-08-05 17:04:09 +0900

    #61 migration修正

--- src/database/migrations/2020_07_30_132236_change_column_offers_table.php
+++ src/database/migrations/2020_07_30_132236_change_column_offers_table.php
@@ -18,17 +18,15 @@ class ChangeColumnOffersTable extends Migration
                 $table->dropForeign('offers_offer_from_id_foreign');
                 $table->dropForeign('offers_offer_to_id_foreign');
             }
+            $table->dropColumn([
+                'offer_from_id',
+                'offer_to_id'
+            ]);
 
             $table->unsignedBigInteger('gym_login_id')->nullable()->after('id')->comment('ジム側のログインID');
             $table->unsignedBigInteger('trainer_login_id')->nullable()->after('gym_login_id')->comment('トレーナー側のログインID');
             $table->foreign('gym_login_id')->references('id')->on('login');
             $table->foreign('trainer_login_id')->references('id')->on('login');
-        });
-        Schema::table('offers', function (Blueprint $table) {
-            $table->dropColumn('offer_from_id');
-        });
-        Schema::table('offers', function (Blueprint $table) {
-            $table->dropColumn('offer_to_id');
         });
     }
 
@@ -44,18 +42,15 @@ class ChangeColumnOffersTable extends Migration
                 $table->dropForeign('offers_gym_login_id_foreign');
                 $table->dropForeign('offers_trainer_login_id_foreign');
             }
-
+            $table->dropColumn([
+                'trainer_login_id',
+                'gym_login_id'
+            ]);
 
             $table->unsignedBigInteger('offer_from_id')->after('id')->comment('オファーしたユーザーのID');
             $table->unsignedBigInteger('offer_to_id')->after('offer_from_id')->comment('オファーされたユーザーのID');
             $table->foreign('offer_from_id')->references('id')->on('login');
             $table->foreign('offer_to_id')->references('id')->on('login');
-        });
-        Schema::table('offers', function (Blueprint $table) {
-            $table->dropColumn('trainer_login_id');
-        });
-        Schema::table('offers', function (Blueprint $table) {
-            $table->dropColumn('gym_login_id');
         });
     }
 }
