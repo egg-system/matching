e6c0e3a174ca4feb3fa3bfbc2073716ad218d960
haji33 <hajime.sasanuma.0828@gmail.com>
2020-08-08 00:40:07 +0900

    feat: パスワード入力ページのデザイン実装

--- src/resources/js/organisms/users-register/index.vue
+++ src/resources/js/organisms/users-register/index.vue
@@ -2,18 +2,20 @@
   <div>
     <start-page v-show="stepState === 0" @moveNext="moveNextStep" />
     <name-step v-show="stepState === 1" @moveNext="moveNextStep" />
-    <user-type-step v-show="stepState === 2" @moveNext="moveNextStep" @back="movePrevStep" />
-    <occupation-step v-show="stepState === 3" :isShown="stepState === 3" :occupations="occupations" @moveNext="moveNextStep" @back="movePrevStep" />
-    <area-step v-show="stepState === 4" :isShown="stepState === 4" :areas="areas" @moveNext="moveNextStep" @back="movePrevStep" />
-    <work-style-step v-show="stepState === 5" :isShown="stepState === 5" :areas="areas" @moveNext="moveNextStep" @back="movePrevStep" />
-    <career-step v-show="stepState === 6" @moveNext="moveNextStep" @back="movePrevStep" />
-    <thanks-page v-show="stepState === 7" @entry="entry" />
+    <password-step v-show="stepState === 2" @moveNext="moveNextStep" @back="movePrevStep" />
+    <user-type-step v-show="stepState === 3" @moveNext="moveNextStep" @back="movePrevStep" />
+    <occupation-step v-show="stepState === 4" :isShown="stepState === 4" :occupations="occupations" @moveNext="moveNextStep" @back="movePrevStep" />
+    <area-step v-show="stepState === 5" :isShown="stepState === 5" :areas="areas" @moveNext="moveNextStep" @back="movePrevStep" />
+    <work-style-step v-show="stepState === 6" :isShown="stepState === 6" :areas="areas" @moveNext="moveNextStep" @back="movePrevStep" />
+    <career-step v-show="stepState === 7" @moveNext="moveNextStep" @back="movePrevStep" />
+    <thanks-page v-show="stepState === 8" @entry="entry" />
   </div>
 </template>
 
 <script>
 import startPage from './start-page'
 import nameStep from './name-step'
+import passwordStep from './password-step'
 import userTypeStep from './user-type-step'
 import occupationStep from './occupation-step'
 import areaStep from './area-step'
@@ -28,12 +30,13 @@ export default {
   },
   data () {
     return {
-      stepState: 0
+      stepState: 2
     }
   },
   components: {
     startPage,
     nameStep,
+    passwordStep,
     userTypeStep,
     occupationStep,
     areaStep,
---
+++ src/resources/js/organisms/users-register/password-step.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="password-step-wrapper">
+    <v-img src="/images/users-register/finish_background.jpg">
+      <div class="password-step">
+        <div class="password-step__header">
+          <a href="javascript:void(0)" @click="back" class="back-link"></a>
+          <span>アカウントのパスワードを設定してください</span>
+        </div>
+
+        <div class="password-form">
+          <div class="password-form__heading">パスワード</div>
+          <div class="password-form__input">
+            <input
+              v-model="inputValue"
+              :type="isShownPassword ? 'text' : 'password'"
+              class="password-form__input"
+              name="password"
+              autocomplete="password"
+              autofocus
+              required
+            >
+            <a href="javascript:void(0)" @click.prevent="toggleIsShownPassword">
+              <v-icon size="20">{{ isShownPassword ? 'mdi-eye-off' : 'mdi-eye' }}</v-icon>
+            </a>
+          </div>
+        </div>
+
+        <rounded-btn class="next-btn" text="次へ" :disabled="!inputValue" @click="moveNext" />
+      </div>
+    </v-img>
+
+    <steps-footer step="1" :progress="10" />
+  </div>
+</template>
+
+<script>
+import roundedBtn from '../../atoms/users-register/rounded-btn'
+import stepsFooter from '../../molecules/users-register/steps-footer'
+
+export default {
+  components: {
+    roundedBtn,
+    stepsFooter
+  },
+  data () {
+    return {
+      inputValue: '',
+      isShownPassword: false
+    }
+  },
+  methods: {
+    moveNext () {
+      this.$emit('moveNext')
+    },
+    back () {
+      this.$emit('back')
+    },
+    toggleIsShownPassword () {
+      this.isShownPassword = !this.isShownPassword
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.password-step-wrapper {
+  .password-step {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    &__header {
+      width: 100%;
+      margin-top: 72px;
+      .back-link {
+        content: "";
+        width: 15px;
+        height: 15px;
+        border-bottom: solid 4px black;
+        border-right: solid 4px black;
+        transform: rotate(135deg);
+        position: absolute;
+        margin-top: -44px;
+        margin-left: 20px;
+        border-radius: 3px;
+      }
+      span {
+        font-weight: bold;
+        font-size: 0.9rem;
+        display: inline-block;
+        width: 100%;
+        text-align: center;
+      }
+    }
+    .password-form {
+      margin-top: 125px;
+      width: 75%;
+      &__heading {
+        font-size: 0.5rem;
+        color: grey;
+      }
+      &__input {
+        display: flex;
+        align-items: baseline;
+        input {
+          width: 100%;
+          border-bottom: solid 1px;
+          font-size: 1.1rem;
+          font-weight: bold;
+          padding: 3px 0 7px 0;
+          margin-right: -25px;
+        }
+        a {
+          text-decoration: unset;
+        }
+      }
+    }
+    .next-btn {
+      margin-top: 60px;
+    }
+  }
+  .steps-footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background: white;
+  }
+}
+</style>
