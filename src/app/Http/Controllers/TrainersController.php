2858f4d18de0fc3e0e07cea2c27b7954d087bc05
hk-pvt <gmxfw068@gmail.com>
2020-08-09 22:36:13 +0900

    fix: 最新master取り込み後のtest修正

--- src/app/Http/Controllers/TrainersController.php
+++ src/app/Http/Controllers/TrainersController.php
@@ -4,20 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TrainerSearchRequest;
 use App\Models\MatchingCondition;
+use App\Services\Search\UserSearchService;
+
+use Illuminate\Support\Facades\Log;
 
 class TrainersController extends Controller
 {
+    /** @var UserSearchService  */
+    protected $userSearchService;
+
+    public function __construct(UserSearchService $userSearchService)
+    {
+        $this->userSearchService = $userSearchService;
+    }
+
     /**
      * トレーナの一覧表示画面(検索付)
      */
     public function index(TrainerSearchRequest $request)
     {
-        $validated = $request->validated();
-        $matchingCondition = MatchingCondition::with(['user', 'area'])->onlyTrainer();
-        if ($request->anyFilled(array_keys($validated))) {
-            $matchingCondition = $matchingCondition->search($validated);
-        }
-        $conditions = $matchingCondition->get();
+        $conditions = $this->userSearchService->execute($request);
         return view('pages.trainers.index', compact('conditions'));
     }
 }
