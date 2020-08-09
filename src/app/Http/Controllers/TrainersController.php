51bacd42f8641f53f85a92c51a572a55894f75e7
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-09 21:25:43 +0900

    feat : trainersControllerの修正

--- src/app/Http/Controllers/TrainersController.php
+++ src/app/Http/Controllers/TrainersController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TrainerSearchRequest;
+use App\Models\MatchingCondition;
+
 class TrainersController extends Controller
 {
     /**
      * トレーナの一覧表示画面(検索付)
      */
-    public function index()
+    public function index(TrainerSearchRequest $request)
     {
         $validated = $request->validated();
         $matchingCondition = MatchingCondition::with(['user', 'area'])->onlyTrainer();
