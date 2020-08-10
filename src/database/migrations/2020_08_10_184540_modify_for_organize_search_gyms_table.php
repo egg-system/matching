8ab5833e02cda09d01de98288998a1f53ed304d0
hk-pvt <gmxfw068@gmail.com>
2020-08-10 21:53:55 +0900

    update: migrationファイル名の修正

--- src/database/migrations/2020_08_10_184540_modify_for_organize_search_gyms_table.php
+++ src/database/migrations/2020_08_10_184540_modify_for_organize_search_gyms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModifyGymsTable extends Migration
+class ModifyForOrganizeSearchGymsTable extends Migration
 {
     /**
      * Run the migrations.
