84a244ca3e6aa3133f7fd41578810489702ee0e4
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-11 21:59:47 +0900

    fix : descriptionが正常に表示されないので、改行を追加

--- src/resources/views/components/layouts/common/meta.blade.php
+++ src/resources/views/components/layouts/common/meta.blade.php
@@ -9,9 +9,5 @@
 @hasSection('description')
     <meta name="description" content="@yield('description')">
 @else
-    <meta name="description" content="
-        BorderlessGYMは、ジムとトレーナーが出会うための複業マッチングプラットフォームです。
-        トレーナーの複業・副業を推進し、会社・組織の境目をなくしていくことで、 
-        より柔軟な働き方を実現し、経済的・精神的な自由を得るためのサービスです。
-    ">
+    <meta name="description" content="BorderlessGYMは、ジムとトレーナーが出会うための複業マッチングプラットフォームです。トレーナーの複業・副業を推進し、会社・組織の境目をなくしていくことで、 より柔軟な働き方を実現し、経済的・精神的な自由を得るためのサービスです。">
 @endif
