06aaa6ef3b0f5ae95783ace18e99e9b108d155f1
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-18 08:22:48 +0900

    fix : リファクタリング

--- src/resources/views/pages/offers/index.blade.php
+++ src/resources/views/pages/offers/index.blade.php
@@ -1,20 +1,38 @@
 @extends('templates.app')
 @push('script')
-<link href="{{ asset('/css/pages/offers/index.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/pages/offers/index.css') }}" rel="stylesheet">
 @endpush
 
 @section('content')
-<div class="offers-index-page">
-    <div class="bg">
-        <h2>エントリー／オファー一覧</h2>
-        @foreach($offers as $offer)
-        <history-item
-          transition="{{$user->isGym ? '/trainers/' . $offer->trainer->user_id : '/gyms/' . $offer->gym->user_id}}"
-          photo="{{$user->isGym ? $offer->trainer->photo_url : $offer->gym->photo_url}}"
-          name="{{$user->isGym ? $offer->trainerName : $offer->gymName}}"
-          comment="{{$offer->created_at->format('Y/m/d')}}に{{$offer->state->name}}{{$user->user_type === $offer->state->transition_user_type ? 'しました' : 'が届きました'}}"
-        ></history-item>
-        @endforeach
-    </div><!-- .bg -->
-</div><!-- .wrapper -->
+    <div class="offers-index-page">
+        <div class="bg">
+            <h2>エントリー／オファー一覧</h2>
+            @forelse($offers as $offer)
+                @php
+
+                    $trainsition = $user->isGym ? "/trainers/{$offer->trainerLogin->user_id}"
+                        : "/gyms/{$offer->gymLogin->user_id}";
+                    $photo = $user->isGym ? '/images/trainers/trainer-icon.png'
+                        : $offer->gymLogin->user->job->main_image_url;
+
+                    $comment = $offer->created_at->format('Y/m/d');
+                    $comment = "{$comment}に{$offer->state->name}";
+
+                    $trainsionUserType = $offer->state->transition_user_type;
+                    $doesLoginUserOffer = $user->user_type === $trainsionUserType;
+                    $comment = $doesLoginUserOffer ? "{$comment}しました" : "{$comment}が届きました";
+
+
+                @endphp
+                <history-item
+                    transition='{{ $trainsition }}'
+                    photo="{{ $photo }}"
+                    name="{{ $user->isGym ? $offer->trainerName : $offer->gymName }}"
+                    comment='{{ $comment }}'
+                ></history-item>
+            @empty
+                <p>オファーはまだありません。</p>
+            @endforelse
+        </div>
+    </div>
 @endsection
