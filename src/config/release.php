a322de6af54bd50992e74498d49de50a61351bc6
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-08-18 10:08:14 +0900

    fix : configの修正

--- src/config/release.php
+++ src/config/release.php
@@ -5,9 +5,9 @@
  */
 return [
     'register' => [
-        'is_enabled' => env('IS_RELEASE_REGISTER', false)
+        'is_enabled' => env('IS_RELEASE_REGISTER', true)
     ],
     'login' => [
-        'is_enabled' => env('IS_RELEASE_LOGIN', false)
+        'is_enabled' => env('IS_RELEASE_LOGIN', true)
     ]
 ];
