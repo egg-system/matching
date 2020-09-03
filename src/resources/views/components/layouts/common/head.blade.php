b30d8022d3ede00bdfa65d7d1e3ca2338804231f
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-09-04 02:17:00 +0900

    fix : ズームインしないように修正

--- src/resources/views/components/layouts/common/head.blade.php
+++ src/resources/views/components/layouts/common/head.blade.php
@@ -1,8 +1,12 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+>
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- iphoneの場合、-のあるテキストはリンクになるため、以下を追加 -->
 <meta name="format-detection" content="telephone=no">
+
